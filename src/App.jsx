69919218b6e176e483056c59828ada104c189bfd
lavindu perera <lavi156perera@gmail.com>
2025-11-08 20:44:43 +0530

    removed unnecessary imports in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/sonner";
-import MenuBar from "@/components/MenuBar";
 import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
