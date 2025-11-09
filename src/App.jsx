ff41d9c69328733eefbc0d81d5afd924bb622f64
lavindu perera <lavi156perera@gmail.com>
2025-11-09 17:11:25 +0530

    updated the projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import ContactMe from "./components/ContactMe";
+import ParticleAnimation from "./components/ParticleAnimation";
 
 function App() {
   return (
     <>
       {/* <MenuBar /> */}
+
       <Hero />
+
       <AboutMe />
       <Projects />
       <ContactMe />
